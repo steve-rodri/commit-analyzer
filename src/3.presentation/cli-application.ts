cb9fed1ea98a6742d61d95cd7ec6d5c09eb5c714
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2025-08-28 18:14:40 +0700

    bump version to 1.1.4

--- src/3.presentation/cli-application.ts
+++ src/3.presentation/cli-application.ts
@@ -24,7 +24,7 @@ export interface CLIOptions {
 }
 
 export class CLIApplication {
-  private static readonly VERSION = "1.1.3"
+  private static readonly VERSION = "1.1.4"
   private static readonly DEFAULT_COMMITS_OUTPUT_FILE = "results/commits.csv"
   private static readonly DEFAULT_REPORT_OUTPUT_FILE = "results/report.md"
 
